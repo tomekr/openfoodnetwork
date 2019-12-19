dd016eb52c643e150e7c049a4c15f2c5c6b36b10
luisramos0 <luisramos0@gmail.com>
2019-12-19 12:21:10 +0000

    Bring and require spin from spree_backend and respective progress indicator js and css

--- app/assets/javascripts/admin/all.js
+++ app/assets/javascripts/admin/all.js
@@ -17,6 +17,7 @@
 //= require admin/spree_backend
 //= require spree
 //= require jquery.vAlign
+//= require spin
 //= require admin/spree_paypal_express
 //= require ../shared/ng-infinite-scroll.min.js
 //= require ../shared/ng-tags-input.min.js
